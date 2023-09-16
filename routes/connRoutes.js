50a9492d6fdc007b30ef3ffa874cb0b7aa2aaa18
d4saher <113984123+d4saher@users.noreply.github.com>
2023-09-16 18:39:35 +0200

    Update connRoutes.js

--- routes/connRoutes.js
+++ routes/connRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const connController = require('../controllers/connController');
 
 router.get('/', connController.getAllConns);
-router.get('/:id', connController.getConn);
+router.get('/:id', connController.getConns);
 router.post('/', connController.createConn);
 router.put('/:id', connController.updateConn);
 router.delete('/:id', connController.deleteConn);
